0ee3fa0674f8fd4b6c05faec41d22b4b8125a497
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-21 20:38:05 +0300

    mortage calculator

--- src/com/basics/calculator/Mortgage.java
+++ src/com/basics/calculator/Mortgage.java
@@ -8,15 +8,26 @@ public class Mortgage {
 
 //        M = P[r(1+r)^n]/[(1+r)^n -1]
 
-        float mortgage;
+        double mortgage;
         int principal;
-        float rate;
+        double AnnualRate = 0.18;
+        double monthlyRate = AnnualRate /12;
         int period;
+
 
         System.out.println("Enter the principal amount:");
         Scanner sc = new Scanner(System.in);
         principal = sc.nextInt();
         System.out.println("The principal amount is: " +principal);
+
+        System.out.println("Enter the period you will take to repay the mortgage:");
+        Scanner so = new Scanner(System.in);
+        period = so.nextInt();
+        System.out.println("The period is: " +period);
+
+        mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, period)) / (Math.pow(1 + monthlyRate, period) - 1);
+
+        System.out.println("The mortgage is: " +mortgage);
 
 
 
