1ca46e747446da6b0e3a3259c2660398c99f8e2a
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-21 18:25:49 +0300

    basics java

---
+++ src/com/basics/second/Main2.java
@@ -0,0 +1,13 @@
+package com.basics.second;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+//       declaring a constant
+
+        final int speed = 40;
+
+        System.out.println(speed);
+
+    }
+}
