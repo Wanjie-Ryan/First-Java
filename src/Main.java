3193a99f9bf854a4f0087741b722023cfde45075
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-21 16:03:47 +0300

    basics java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
         int age = 30;
+        int views = 145_768;
+        double decNum = 12.44;
+        boolean isAdult = false;
+//        float floNum = 12.4455f;
+
+        Date now = new Date();
+        System.out.println(now);
+        now.getYear();
+
+        String text = "my name is ryan";
+        String conc = text + "I am wewe years";
+        System.out.println(conc);
 
 
         System.out.println(age);
+        System.out.println(views);
 
     }
 }
